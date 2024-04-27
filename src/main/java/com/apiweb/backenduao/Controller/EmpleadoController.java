8048c9f37aef88c7afb80b597e5e051d777029d0
danielscz <danielscz.dsa@gmail.com>
2024-04-26 19:04:19 -0500

    Get employees by office, in the endPoint /apiweb/v1/empleados/sede/{idSede}

--- src/main/java/com/apiweb/backenduao/Controller/EmpleadoController.java
+++ src/main/java/com/apiweb/backenduao/Controller/EmpleadoController.java
@@ -1,5 +1,6 @@
 package com.apiweb.backenduao.Controller;
 
+import com.apiweb.backenduao.Exception.RecursoNoEncontradoExcep;
 import com.apiweb.backenduao.Model.EmpleadoModel;
 import com.apiweb.backenduao.Service.IEmpleadoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,17 @@ public class EmpleadoController {
     @GetMapping("/")
     public ResponseEntity<List<EmpleadoModel>> mostrarEmpleados() {
         return new ResponseEntity<>(empleadoService.obtenerEmpleados(), HttpStatus.OK);
+    }
+
+    @GetMapping("/sede/{id}")
+    public ResponseEntity<?> mostrarEmpleadosSede(@PathVariable int id) {
+        try {
+            List<EmpleadoModel> empleados = empleadoService.obtenerEmpleadosSede(id);
+            return ResponseEntity.ok(empleados);
+        } catch (RecursoNoEncontradoExcep e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
     }
 
 }
---
+++ src/main/java/com/apiweb/backenduao/Repository/ISedeRepository.java
@@ -0,0 +1,7 @@
+package com.apiweb.backenduao.Repository;
+
+import com.apiweb.backenduao.Model.SedeModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ISedeRepository extends JpaRepository<SedeModel, Integer> {
+}
